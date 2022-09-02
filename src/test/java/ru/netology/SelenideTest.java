2914076be0e42271673910123c7d025b2b7d25a7
dmitriysokolov94 <ipsokolov2021@gmail.com>
2022-09-02 11:10:26 +0300

    data

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -3,10 +3,9 @@ package ru.netology;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,11 +25,9 @@ public class SelenideTest {
         Selenide.open("http://localhost:9999");
     }
 
-    public String getDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, +4);
-        return dateFormat.format(calendar.getTime());
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     }
 
 
@@ -39,16 +36,18 @@ public class SelenideTest {
 
         Configuration.holdBrowserOpen = true;
 
+        String planningDate = generateDate(4);
+
         $("[data-test-id=city] input").setValue("Псков");
         $("[data-test-id=date] .input__control").click();
         $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] .input__control").sendKeys(BACK_SPACE);
-        $("[data-test-id=date] .input__control").setValue(getDate());
+        $("[data-test-id=date] .input__control").setValue(planningDate);
         $("[data-test-id=name] .input__control").setValue("Иван Иванов");
         $("[data-test-id=phone] .input__control").setValue("+79997894564");
         $("[data-test-id=agreement]").click();
         $x("//button[contains(@class, 'button_view_extra')]").click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + getDate()));
+        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
